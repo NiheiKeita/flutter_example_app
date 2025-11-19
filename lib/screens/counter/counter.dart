0b5e9e7d0f3d8675c9db430df19bee5b62488414
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2025-11-19 17:40:12 +0900

    カウンター画面をステートフルに変更し、カウントアップ機能を実装

--- lib/screens/counter/counter.dart
+++ lib/screens/counter/counter.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int count = 0;
+
+  void incrementCounter() {
+    setState(() => count++);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('CounterScreen')),
-      body: const Center(child: Text('Counter Screen')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text('$count'),
+            TextButton(
+              onPressed: incrementCounter,
+              child: const Text('カウントアップ'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
